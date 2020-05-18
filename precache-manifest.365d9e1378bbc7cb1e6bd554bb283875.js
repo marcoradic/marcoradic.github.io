2a27847125dad42ad126874b0fcf1799b4d28422
Marco Radic <marco.radic@targens.de>
2020-05-18 15:45:39 +0200

    Updates

---
+++ precache-manifest.365d9e1378bbc7cb1e6bd554bb283875.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07ff234a3a485de32e0d3c6946aa6ded",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1bc18088ca848fbda02b",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0b1c1b8b93d78535d018",
+    "url": "/static/js/2.15931a8f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.15931a8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bc18088ca848fbda02b",
+    "url": "/static/js/main.e08cb1d7.chunk.js"
+  },
+  {
+    "revision": "967a46d8b67e4e01db1f",
+    "url": "/static/js/runtime-main.7e4b9196.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
